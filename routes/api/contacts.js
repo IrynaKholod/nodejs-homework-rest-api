8ee25455d5341091480f20fff739e97fa2d3c3e0
IrynaKholod <104202392+IrynaKholod@users.noreply.github.com>
2023-03-04 22:00:42 +0200

    додає auth midlwares

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -8,15 +8,15 @@ const {schemas} = require("../../models/contact")
 
 router.get("/", auth, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", isValidId, ctrlWrapper(ctrl.getById));
+router.get("/:contactId", auth, isValidId, ctrlWrapper(ctrl.getById));
 
 router.post("/", auth, validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
 
-router.put("/:contactId", isValidId, validateBody(schemas.updateSchema), ctrlWrapper(ctrl.updateById));
+router.put("/:contactId", auth, isValidId, validateBody(schemas.updateSchema), ctrlWrapper(ctrl.updateById));
 
-router.patch("/:id/favorite", isValidId, validateBody(schemas.updateFavoriteSchema), ctrlWrapper(ctrl.updateFavorite));
+router.patch("/:id/favorite", auth, isValidId, validateBody(schemas.updateFavoriteSchema), ctrlWrapper(ctrl.updateFavorite));
 
-router.delete("/:contactId", isValidId,  ctrlWrapper(ctrl.removeById));
+router.delete("/:contactId",  auth, isValidId,  ctrlWrapper(ctrl.removeById));
 
 
 
